3955ca9cfb7f49cafe22bc54487e5b1efbf83021
HelenOne <h.bright@yandex.ru>
2020-04-10 11:55:36 +0300

    init commit

---
+++ modules/example_module/example_fucntion_name.h
@@ -0,0 +1,6 @@
+#ifndef EXAMPLE_FUNCTION_NAME
+#define EXAMPLE_FUNCTION_NAME
+
+int example_function(int exampleAttr1, int exampleAttr2);
+
+#endif
---
+++ modules/example_module/example_function_name.cpp
@@ -0,0 +1,4 @@
+int example_function(int exampleAttr1, int exampleAttr2)
+{
+  return 0;
+}
