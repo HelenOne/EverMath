43e7bcfc9ce85fc955c434b416b12858f173355e
Andrew Ch <63413425+Andy-rew@users.noreply.github.com>
2020-04-19 21:19:15 +0300

    Add files via upload

---
+++ COM_NN_D.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std; 
+
+void comparison(char arr[], const int size)
+{
+	int i;
+	for (i = 0; i < size-1; i++)
+	{
+		if (arr[i] > arr[i + 1])
+		{
+			cout << "Число" << arr[i] << "больше" << arr[i + 1];
+		}
+		if (arr[i] < arr[i + 1])
+		{
+			cout << "Число" << arr[i] << "меньше" << arr[i+1];
+		}
+		else
+		{
+			cout << "Числа" << arr[i] << "и" << arr[i + 1] << "равны";
+		}
+	}
+}
+
+int main()
+{
+	const int size = 10;
+	char arr[size];
+}
