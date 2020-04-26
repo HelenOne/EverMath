cf693a6d68cc186e88710fa3a515d24474eed544
HelenOne <h.bright@yandex.ru>
2020-04-27 00:59:18 +0300

    commented WA code to prevent errors

--- frontend/pages/naturals/plus_one.js
+++ frontend/pages/naturals/plus_one.js
@@ -6,21 +6,21 @@ import { Input } from "antd";
 const plus_one = () => {
   const [numberValue, setNumberValue] = React.useState("");
 
-  const [wasm, setWasm] = React.useState(null);
-  React.useEffect(() => {
-    import("../../../modules/N3/ADD_1N_N.wasm").then(setWasm);
-  }, [setWasm]);
-  const result = React.useMemo(() => {
-    if (wasm === null) return null;
-    // const ADD_1N_N = wasm._Z8ADD_1N_NPii;
-    // if (numberValue !== "") {
-    //   const numberArr = numberValue.split("");
-    //   for (let i = 0; i < numberArr.length; i++) {
-    //     numberArr[i] = parseInt(numberArr[i]);
-    //   }
-    //   return ADD_1N_N(numberArr, numberArr.length);
-    // }
-  }, [wasm, numberValue]);
+  // const [wasm, setWasm] = React.useState(null);
+  // React.useEffect(() => {
+  //   import("../../../modules/N3/ADD_1N_N.wasm").then(setWasm);
+  // }, [setWasm]);
+  // const result = React.useMemo(() => {
+  //   if (wasm === null) return null;
+  // const ADD_1N_N = wasm._Z8ADD_1N_NPii;
+  // if (numberValue !== "") {
+  //   const numberArr = numberValue.split("");
+  //   for (let i = 0; i < numberArr.length; i++) {
+  //     numberArr[i] = parseInt(numberArr[i]);
+  //   }
+  //   return ADD_1N_N(numberArr, numberArr.length);
+  // }
+  // }, [wasm, numberValue]);
 
   return (
     <div>
@@ -29,7 +29,7 @@ const plus_one = () => {
         onChange={(event) => setNumberValue(event.target.value)}
         placeholder="Введите число"
       />
-      <div>{result}</div>
+      <div>{/*result*/}</div>
     </div>
   );
 };
