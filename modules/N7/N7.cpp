56b2eb198fc5f3ebef80fe795585590fc838075d
Андрей Чернышев <andy_1505@mail.ru>
2020-04-26 23:12:48 +0300

    done

---
+++ modules/N7/N7.cpp
@@ -0,0 +1,15 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+char* MUL_Nk_N (int size, char* num, int k) // N7
+{
+	num = (char*)realloc(num, sizeof(char) * (size + k));
+	for (int i = size; i < size + k; ++i)
+	{
+		num[i] = 0;
+	}
+	return num;
+}
---
+++ modules/N7/N7.h
@@ -0,0 +1,5 @@
+#ifndef MUL_Nk_N
+#define MUL_Nk_N
+
+char* MUL_Nk_N(int size, char* num, int k);
+#endif
