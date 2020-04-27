aad9edcadaf856e16a9f4094c6571c753b5dd4ea
R-slav <63425007+R-slav@users.noreply.github.com>
2020-04-27 23:21:31 +0600

    Update LED_P_Q.cpp

--- modules/polynomials/P5/LED_P_Q.cpp
+++ modules/polynomials/P5/LED_P_Q.cpp
@@ -1,12 +1,10 @@
 #include"LED_P_Q.h"
 #include<iostream>
 using namespace std;
-int **LED_P_Q(int **array_of_terms, long int count_of_terms) {
+int **LED_P_Q(int **array_of_terms, long int count_of_terms) { // вернтуть коэффициент старшего разряда
 	for (int i = count_of_terms - 1; i >= 0; i--) {
-		if (array_of_terms[i][1] != 0) {
-			cout << &array_of_terms[i]<< endl;
-			
-			return &array_of_terms[i];
+		if (array_of_terms[i][1] != 0) { // 0-ой элемент - знак коэффициента
+			return &array_of_terms[i]; // Возврат ссылки на 0-ой элемента
 		}
 	}
 }
