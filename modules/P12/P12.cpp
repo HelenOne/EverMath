430fa209dd015bf15a3ea148468720aeeb4977ea
Андрей Чернышев <andy_1505@mail.ru>
2020-04-27 17:01:46 +0300

    done

---
+++ modules/P12/P12.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+int* DER_P_P (int size, int* mas) // P12
+{
+	
+	for (int i = 1; i < size; ++i)
+	{
+		mas[i - 1] = mas[i] * i;
+	}
+	mas[size - 1] = 0;
+	return mas;
+}
---
+++ modules/P12/P12.h
@@ -0,0 +1,7 @@
+#ifndef DER_P_P
+#define DER_P_P
+
+int* DER_P_P(int size, int* mas);
+
+#endif
+
