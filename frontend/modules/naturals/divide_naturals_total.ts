f825d5045b65ba94baa322869604d8aa7d03de9b
vladarh <vladarh11v@gmail.com>
2020-05-04 00:52:05 +0300

    Update divide_naturals_total.ts

--- frontend/modules/naturals/divide_naturals_total.ts
+++ frontend/modules/naturals/divide_naturals_total.ts
@@ -23,53 +23,12 @@ export const divideNaturalsTotal = (a: string, b: string) => {
     return '';
   }
 
-  let result: string = ''; //неполное частное
-  let position = 0; //какая цифра по счету от начала
-
-  // compareNaturals((multiplyNaturals(result.join(''), bArr.join('')) + rest), bArr.join('')) === 1
-
-  let aWorkingPart = a.substr(position, b.length); //кусочек делимого, который сейчас делим
-  while (true) {
-    let workingPartExtended = false;
-    const initWorkingPart = aWorkingPart;
-    for (let i = 1; compareNaturals(aWorkingPart, b) === 1; i++) {
-      aWorkingPart =
-        initWorkingPart + a.substr(position + initWorkingPart.length, i);
-      aWorkingPart = aWorkingPart.replace(/^0+/, '');
-
-      if (workingPartExtended) {
-        result += '0';
-      }
-      workingPartExtended = true;
-      if (position + initWorkingPart.length + i >= a.length) {
-        break;
-      }
-    }
-    let rate = 0;
-    let buffer = b;
-    while (true) {
-      const possibleRate = rate + 1;
-      const possibleBuffer = multiplyNaturals(b, possibleRate.toString());
-      const comparing = compareNaturals(possibleBuffer, aWorkingPart);
-      if (comparing === 2) {
-        break;
-      }
-      rate = possibleRate;
-      buffer = possibleBuffer;
-    }
-    result += rate.toString();
-    if (aWorkingPart.length === 0) {
-      break;
-    }
-    const edgeReached = position + aWorkingPart.length >= a.length;
-    let newWorkingPart = subtractNaturals(aWorkingPart, buffer);
-    newWorkingPart = newWorkingPart.replace(/^0+/, '');
-    position += aWorkingPart.length - newWorkingPart.length;
-    aWorkingPart = newWorkingPart;
-    if (edgeReached && compareNaturals(aWorkingPart, b) === 1) {
-      break;
-    }
-  }
-
+   let result: string = '0'; //неполное частное
+   let sub: string = a;
+   let result: string; 
+   while(compareNaturals(sub,b) !== 1){
+   sub = substr(sub, b);
+   result = plusOne(result);
+   }
   return result;
 };
