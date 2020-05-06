ad42a20a58f988928ff2297e9f493f9181b7f318
HelenOne <h.bright@yandex.ru>
2020-05-06 12:55:48 +0300

    fixed integers division rest

--- frontend/modules/integers/divide_integers_mod.ts
+++ frontend/modules/integers/divide_integers_mod.ts
@@ -4,7 +4,19 @@ import { divideNaturalsTotal } from '../naturals/divide_naturals_total';
 import { absInteger } from './abs_integer';
 import { multiplyNaturals } from '../naturals/multiply_naturals';
 import { getTheRestOfDivisionNaturals } from '../naturals/get_the_rest_of_division_naturals';
+import { subtractNaturals } from '../naturals/subtract_naturals';
+import { compareNaturals } from '../naturals/compare_naturals';
+import { divideIntegersTotal } from './divide_integers_total';
 
 export const divideIntegersMod = (a: string, b: string) => {
-  return getTheRestOfDivisionNaturals(absInteger(a), absInteger(b));
+  if (compareNaturals(a, b) === 1) {
+    let c = a;
+    a = b;
+    b = c;
+  }
+  // return getTheRestOfDivisionNaturals(absInteger(a), absInteger(b));
+  return subtractNaturals(
+    absInteger(a),
+    multiplyNaturals(absInteger(b), absInteger(divideIntegersTotal(a, b)))
+  );
 };
