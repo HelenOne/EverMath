f23b464addd3f201597af862652c7619684efa2e
HelenOne <h.bright@yandex.ru>
2020-05-06 10:07:40 +0300

    added considering zero in the rest

--- frontend/modules/integers/divide_integers_total.ts
+++ frontend/modules/integers/divide_integers_total.ts
@@ -5,11 +5,14 @@ import { changeSign } from './change_sign';
 import { divideNaturalsTotal } from '../naturals/divide_naturals_total';
 import { absInteger } from './abs_integer';
 import { plusOne } from '../naturals/plus_one';
+import { getTheRestOfDivisionNaturals } from '../naturals/get_the_rest_of_division_naturals';
 
 export const divideIntegersTotal = (a: string, b: string) => {
   if (isPositive(a) !== isPositive(b)) {
     return changeSign(
-      plusOne(divideNaturalsTotal(absInteger(a), absInteger(b)))
+      getTheRestOfDivisionNaturals(absInteger(a), absInteger(b)) === '0'
+        ? divideNaturalsTotal(absInteger(a), absInteger(b))
+        : plusOne(divideNaturalsTotal(absInteger(a), absInteger(b)))
     );
   } else return divideNaturalsTotal(absInteger(a), absInteger(b));
 };
